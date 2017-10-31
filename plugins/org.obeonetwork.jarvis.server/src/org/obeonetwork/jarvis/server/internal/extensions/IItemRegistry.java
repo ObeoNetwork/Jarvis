7c00ab5f540affe6a25e5d3ce8b5645645ee21e7
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2017-10-31 17:23:37 +0100

    [11] Improve the branding and the license
    Bug: https://github.com/ObeoNetwork/Jarvis/issues/11
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@gmail.com>

--- plugins/org.obeonetwork.jarvis.server/src/org/obeonetwork/jarvis/server/internal/extensions/IItemRegistry.java
+++ plugins/org.obeonetwork.jarvis.server/src/org/obeonetwork/jarvis/server/internal/extensions/IItemRegistry.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) 2015 Obeo.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
  *
  * Contributors:
  *    Obeo - initial API and implementation
@@ -33,8 +33,7 @@ public interface IItemRegistry<T> {
 	 *
 	 * @param id
 	 *            The identifier
-	 * @return The {@link IItemDescriptor} with the given identifier or null if
-	 *         none could be found
+	 * @return The {@link IItemDescriptor} with the given identifier or null if none could be found
 	 */
 	IItemDescriptor<T> getItemDescriptor(String id);
 
@@ -43,9 +42,8 @@ public interface IItemRegistry<T> {
 	 *
 	 * @param descriptor
 	 *            The descriptior
-	 * @return The previous {@link IItemDescriptor} with the same identifier, or
-	 *         null if no registered {@link IItemDescriptor} had the same
-	 *         identifier
+	 * @return The previous {@link IItemDescriptor} with the same identifier, or null if no registered
+	 *         {@link IItemDescriptor} had the same identifier
 	 */
 	IItemDescriptor<T> add(IItemDescriptor<T> descriptor);
 
@@ -54,8 +52,8 @@ public interface IItemRegistry<T> {
 	 *
 	 * @param id
 	 *            The identifier
-	 * @return The {@link IItemDescriptor} removed or null if no registered
-	 *         {@link IItemDescriptor} had an identifier matching the given one
+	 * @return The {@link IItemDescriptor} removed or null if no registered {@link IItemDescriptor} had an identifier
+	 *         matching the given one
 	 */
 	IItemDescriptor<T> remove(String id);
 
