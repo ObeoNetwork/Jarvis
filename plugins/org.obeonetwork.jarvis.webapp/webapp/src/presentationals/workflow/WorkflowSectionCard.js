fe1e102373927aa1c9371c1ba8fd63dfe06eab09
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2017-11-06 11:25:56 +0100

    [4] Add support for the click on list item icons
    Bug: https://github.com/ObeoNetwork/Jarvis/issues/4
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@gmail.com>

--- plugins/org.obeonetwork.jarvis.webapp/webapp/src/presentationals/workflow/WorkflowSectionCard.js
+++ plugins/org.obeonetwork.jarvis.webapp/webapp/src/presentationals/workflow/WorkflowSectionCard.js
@@ -1,34 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Body, Card, Divider, Header, ItemImgAvatar, ItemSvgIcon, Item, ItemText, List, Title } from "../cards/Card";
 
 import createDiagramImage from './DiagramCreationDescription.gif';
 import './WorkflowSectionCard.css';
 
-const WorkflowSectionCard = (props) => {
-  return (
-    <Card className="workflow-section-card">
-      <Header>
-        <Title>{props.section.title}</Title>
-      </Header>
-      <Divider />
-      <Body>
-        <List>
-          {Object.entries(props.section.activities).map(activityEntry => {
-            return (
-              <Item key={activityEntry[0]}>
-                <ItemImgAvatar src={createDiagramImage} alt='Create a new diagram' />
-                <ItemText>{activityEntry[1].title}</ItemText>
-                <ItemSvgIcon>
-                  <path d="M50,5C25.1,5,5,25.1,5,50c0,24.9,20.1,45,45,45s45-20.1,45-45C95,25.1,74.9,5,50,5z M38.8,69.5v-39L72.5,50L38.8,69.5z"/>
-                </ItemSvgIcon>
-              </Item>
-            );
-          })}
-        </List>
-      </Body>
-    </Card>
-  );
+class WorkflowSectionCard extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    return (
+      <Card className="workflow-section-card">
+        <Header>
+          <Title>{this.props.section.title}</Title>
+        </Header>
+        <Divider />
+        <Body>
+          <List>
+            {Object.entries(this.props.section.activities).map(activityEntry => {
+              return (
+                <Item key={activityEntry[0]}>
+                  <ItemImgAvatar src={createDiagramImage} alt='Create a new diagram' />
+                  <ItemText>{activityEntry[1].title}</ItemText>
+                  <ItemSvgIcon handleClick={(event) => this.handleClick(event, activityEntry[1])}>
+                    <path d="M50,5C25.1,5,5,25.1,5,50c0,24.9,20.1,45,45,45s45-20.1,45-45C95,25.1,74.9,5,50,5z M38.8,69.5v-39L72.5,50L38.8,69.5z"/>
+                  </ItemSvgIcon>
+                </Item>
+              );
+            })}
+          </List>
+        </Body>
+      </Card>
+    );
+  }
+
+  handleClick(event, activity) {
+    console.log(activity);
+  }
 };
 
 export default WorkflowSectionCard;
